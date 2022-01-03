3e41b763ce0685c38391acf8fd9dc0cc624e25a3
Alexander McLean <alexandermclean@Alexanders-iMac.local>
2022-01-03 09:19:49 +0000

    adds a Icon List component and a JSON file for the languages

---
+++ src/components/IconList.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const IconListStyles = styled.div`
+  width: 80%;
+  max-width: 1000px;
+  margin: 50px auto;
+  text-align: center;
+
+  h2 {
+  }
+
+  .icon {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+  }
+`;
+
+const IconStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    height: 50px;
+    margin: 0 auto 10px;
+  }
+  h3 {
+    color: var(--green);
+    font-weight: 300;
+    font-size: 1rem;
+    text-align: center;
+  }
+`;
+
+function SingleIcon({ language }) {
+  return (
+    <IconStyles>
+      <img src={language.image} alt={language.title} />
+      <h3>{language.title}</h3>
+    </IconStyles>
+  );
+}
+
+export default function IconList({ languages }) {
+  return (
+    <IconListStyles>
+      <h2>Languages I've worked with</h2>
+      <div className="icon">
+        {languages.map((language) => (
+          <SingleIcon key={language.id} language={language} />
+        ))}
+      </div>
+    </IconListStyles>
+  );
+}
---
+++ src/data/languages.js
@@ -0,0 +1,47 @@
+var data = {
+  icons: [
+    {
+      id: 1,
+      title: "C#",
+      image: "https://cdn.worldvectorlogo.com/logos/c--4.svg",
+    },
+    {
+      id: 2,
+      title: "React",
+      image: "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      image: "https://cdn.worldvectorlogo.com/logos/logo-javascript.svg",
+    },
+    {
+      id: 4,
+      title: "HTML",
+      image: "https://cdn.worldvectorlogo.com/logos/html5-2.svg",
+    },
+    {
+      id: 5,
+      title: "CSS",
+      image: "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+    },
+    {
+      id: 6,
+      title: "Liquid",
+      image: "https://cdn.worldvectorlogo.com/logos/shopify.svg",
+    },
+    {
+      id: 7,
+      title: "Git",
+      image: "https://cdn.worldvectorlogo.com/logos/git-icon.svg",
+    },
+    {
+      id: 8,
+      title: "Azure",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/f/fa/Microsoft_Azure.svg",
+    },
+  ],
+};
+
+export default data;
