9c81d1002eece603cbedec4df2eded02865500bf
Alexander McLean <alexandermclean@Alexanders-iMac.local>
2022-01-03 10:45:06 +0000

    changes styling to button

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import WorkExperience from "../components/WorkExperience";
 import PageTitle from "../components/PageTitle";
+import IconList from "../components/IconList";
+import data from "../data/languages";
 
 const PageBodyStyles = styled.section`
   h2 {
@@ -14,7 +16,7 @@ const PageBodyStyles = styled.section`
     max-width: 250px;
     border: solid 1px var(--green);
     border-radius: 5px;
-    display: flex; 
+    display: flex;
     flex-direction: column;
     padding: 0 50px;
     h3 {
@@ -29,22 +31,21 @@ const PageBodyStyles = styled.section`
     }
   }
 
-  a {
-    background-color: rgba(0,0,0,0);
+  button {
+    display: inline-block;
+    font-family: var(--mono-font);
     border: solid 1px var(--green);
-    border-radius: 5px;
+    background-color: transparent;
     color: var(--green);
-    font-weight: 500;
-    font-size: 1.05em;
-    letter-spacing: 1px;
-    text-decoration: none;
-    padding: 20px;
+    padding: 15px;
     cursor: pointer;
     transition: all 0.5s;
-    span{
+    line-height: 18px;
+    span {
       padding-left: 10px;
       position: relative;
       top: 5px;
+      font-size: 20px !important;
     }
     &:hover {
       color: var(--navy);
@@ -70,52 +71,75 @@ const PageBodyStyles = styled.section`
 `;
 
 export default function About(props) {
+  const languages = data.icons;
   return (
     <>
-    <PageTitle text="About Me" />
-    <PageBodyStyles>
-      <div className="info-container">
-        <div>
-          <h2>Who am I<span>?</span></h2>
-          <p>
-            Hello! My name is Alex and I love to create things that live on the internet.
-            My interests in web development started in 2019 when I tried to build my wife a blog on wordpress -
-            I thought it would be easy... I was wrong! 
-          </p>
-          <p>
-            Since that day both my wife and I learnt a lot about web design and development, 
-            which led to us starting a freelancing buinsess building websites on the Shopify platform for small businesses.
-          </p>
-          <p>
-            I was always itching to continue my learning, and branched out into learning C#, the .NET environment and how to create server side web applications, 
-            which is where I found my passion, and now want to pursue software engineering as a career.
-          </p>
-          <a href="../assets/b-logo-small.png" download>
-            Download Cv 
-            <span className="material-icons">file_download</span>  
-          </a>
+      <PageTitle text="About Me" />
+      <PageBodyStyles>
+        <div className="info-container">
+          <div>
+            <h2>
+              Who am I<span>?</span>
+            </h2>
+            <p>
+              Hello! My name is Alex and I love to create things that live on
+              the internet. My interests in web development started in 2019 when
+              I tried to build my wife a blog on wordpress - I thought it would
+              be easy... I was wrong!
+            </p>
+            <p>
+              Since that day both my wife and I learnt a lot about web design
+              and development, which led to us starting a freelancing buinsess
+              building websites on the Shopify platform for small businesses.
+            </p>
+            <p>
+              I was always itching to continue my learning, and branched out
+              into learning C#, the .NET environment and how to create server
+              side web applications, which is where I found my passion, and now
+              want to pursue software engineering as a career.
+            </p>
+            <button href="../assets/b-logo-small.png" download>
+              Download Cv
+              <span className="material-icons">file_download</span>
+            </button>
+          </div>
+          <div className="box-border">
+            <h3>Information</h3>
+            <h4>Name:</h4>
+            <p>Alex McLean</p>
+            <h4>Location:</h4>
+            <p>Northamptonshire, UK</p>
+            <h4>Phone:</h4>
+            <p>07443577832</p>
+            <h4>Email:</h4>
+            <p>alexmcl@hotmail.co.uk</p>
+          </div>
         </div>
-        <div className="box-border">
-          <h3>Information</h3>
-          <h4>Name:</h4>
-          <p>Alex McLean</p>
-          <h4>Location:</h4>
-          <p>Northamptonshire, UK</p>
-          <h4>Phone:</h4>
-          <p>07443577832</p>
-          <h4>Email:</h4>
-          <p>alexmcl@hotmail.co.uk</p>
-        </div>
-      </div>
         <h2>Experience & Education</h2>
-      <div className="experience-container">
-        <WorkExperience date="2019 - Present" name="E-commerce Artisans - Freelance" description="Created themes and custom development of features on the Shopify platform" />
-        <WorkExperience date="2018 - 2019" name="Rectory Nusesry - Deputy Manager" description="Managed a team of 6 within the nursery setting and assisted in overall management of the nursery" />
-        <WorkExperience date="2017 - 2020" name="Univerity of Hertfordshire - Bachelor of Arts" description="BA in Early Childhood and Education, I completed this while working full time." />
-        <WorkExperience date="2012 - 2018" name="Various Roles within the Early years Sector" description="I undertook a number of roles within the early years sector helping develop high quality teaching and learning experiences" />
-      </div>
-      
-    </PageBodyStyles>
+        <div className="experience-container">
+          <WorkExperience
+            date="2019 - Present"
+            name="E-commerce Artisans - Freelance"
+            description="Created themes and custom development of features on the Shopify platform"
+          />
+          <WorkExperience
+            date="2018 - 2019"
+            name="Rectory Nusesry - Deputy Manager"
+            description="Managed a team of 6 within the nursery setting and assisted in overall management of the nursery"
+          />
+          <WorkExperience
+            date="2017 - 2020"
+            name="Univerity of Hertfordshire - Bachelor of Arts"
+            description="BA in Early Childhood and Education, I completed this while working full time."
+          />
+          <WorkExperience
+            date="2012 - 2018"
+            name="Various Roles within the Early years Sector"
+            description="I undertook a number of roles within the early years sector helping develop high quality teaching and learning experiences"
+          />
+        </div>
+        <IconList languages={languages} />
+      </PageBodyStyles>
     </>
   );
 }
