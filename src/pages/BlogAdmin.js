14b0f45287d6478f2de8871f60bcfbd1a8e9876c
Alex <alexmcl@hotmail.co.uk>
2022-01-14 07:21:59 +0000

    Adds Alert functionality on status of New Post

--- src/pages/BlogAdmin.js
+++ src/pages/BlogAdmin.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 import Button from "../components/Button";
 import PageTitle from "../components/PageTitle";
 import NewPosts from "../components/NewPosts";
+import PostList from "../components/PostList";
 
 const AdminPanelStyles = styled.div``;
 
@@ -21,13 +24,21 @@ export default function BlogAdmin() {
 
   return (
     <>
-      <PageTitle text="Blog Admin" />
-      <AdminPanelStyles>
-        <div className="Navigation-button">{button}</div>
-      </AdminPanelStyles>
-      <Routes>
-        <Route path="/new" element={<NewPosts />} />
-      </Routes>
+      <AlertProvider
+        template={AlertTemplate}
+        position={positions.MIDDLE}
+        transition={transitions.FADE}
+        timeout="5000"
+      >
+        <PageTitle text="Blog Admin" />
+        <AdminPanelStyles>
+          <div className="Navigation-button">{button}</div>
+        </AdminPanelStyles>
+        <Routes>
+          <Route path="/new" element={<NewPosts />} />
+          <Route path="/" element={<PostList />} />
+        </Routes>
+      </AlertProvider>
     </>
   );
 }
