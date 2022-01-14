93e0f97391077694f10f6af03b606f8431cf940b
Alex <alexmcl@hotmail.co.uk>
2022-01-14 07:23:54 +0000

    changes function away from an anonymous function

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-export default (posts = [], action) => {
+export default function posts(posts = [], action) {
   switch (action.type) {
     case "FETCH_ALL":
       return action.payload;
@@ -7,4 +7,4 @@ export default (posts = [], action) => {
     default:
       return postMessage;
   }
-};
+}
